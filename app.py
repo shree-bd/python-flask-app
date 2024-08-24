c10b75008de9b2ec856c3723eebae45c426040be
shridharbhardwaj <shridharbhardwaj@gmail.com>
2024-08-24 15:14:19 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+# create the flask app
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "<h2>Hello, World!</h2>"
+
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome to the Basic Flask App"
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "The person has passed, and the score is " + str(score)
+
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The person has failed, and the score is " + str(score)
+
+
+@app.route('/calculate', methods=['POST', 'GET'])
+def calculate():
+    if request.method == 'GET':
+        return render_template('calculate.html')
+    else:
+        maths = float(request.form['maths'])
+        science = float(request.form['science'])
+        history = float(request.form['history'])
+
+        average_marks = (maths + science + history) / 3
+        average_marks = round(average_marks, 2)
+        result = "success" if average_marks >= 50 else "fail"
+
+        return render_template('result.html', results=average_marks)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
